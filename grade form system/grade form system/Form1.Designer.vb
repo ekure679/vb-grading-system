43d48084dfee22de4b0e84d56e6a4c79e833abfa
Ekure Joseph <145766878+ekure679@users.noreply.github.com>
2024-09-30 11:35:08 +0100

    Add files via upload

---
+++ grade form system/grade form system/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        lblGrade = New Label()
+        TxtMarks = New TextBox()
+        txtGrades = New TextBox()
+        btnEvaluate = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(340, 37)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(89, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Grading system"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(169, 120)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(69, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Enter marks"
+        ' 
+        ' lblGrade
+        ' 
+        lblGrade.AutoSize = True
+        lblGrade.Location = New Point(169, 178)
+        lblGrade.Name = "lblGrade"
+        lblGrade.Size = New Size(43, 15)
+        lblGrade.TabIndex = 2
+        lblGrade.Text = "Grades"
+        ' 
+        ' TxtMarks
+        ' 
+        TxtMarks.Location = New Point(303, 120)
+        TxtMarks.Name = "TxtMarks"
+        TxtMarks.Size = New Size(324, 23)
+        TxtMarks.TabIndex = 3
+        ' 
+        ' txtGrades
+        ' 
+        txtGrades.Location = New Point(303, 178)
+        txtGrades.Name = "txtGrades"
+        txtGrades.Size = New Size(324, 23)
+        txtGrades.TabIndex = 4
+        ' 
+        ' btnEvaluate
+        ' 
+        btnEvaluate.Location = New Point(317, 228)
+        btnEvaluate.Name = "btnEvaluate"
+        btnEvaluate.Size = New Size(304, 34)
+        btnEvaluate.TabIndex = 5
+        btnEvaluate.Text = "Evaluate"
+        btnEvaluate.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnEvaluate)
+        Controls.Add(txtGrades)
+        Controls.Add(TxtMarks)
+        Controls.Add(lblGrade)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblGrade As Label
+    Friend WithEvents TxtMarks As TextBox
+    Friend WithEvents txtGrades As TextBox
+    Friend WithEvents btnEvaluate As Button
+
+End Class
---
+++ grade form system/grade form system/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+    Private Sub btnEvaluate_Click(sender As Object, e As EventArgs) Handles btnEvaluate.Click
+        Dim marks As Integer
+
+        'input validation for marks to ensure numeric input
+        If Not Integer.TryParse(TxtMarks.Text, marks) Then
+            MessageBox.Show("Please enter  Valid numeric value for marks")
+        End If
+
+        If marks > 100 Then
+            MessageBox.Show("wrong entry, please try again")
+
+        End If
+        ' using select case method to input marks
+        Select Case marks
+            Case 90 To 100
+                txtGrades.Text = "Excellent"
+
+            Case 80 To 89
+                txtGrades.Text = "very good"
+
+            Case 70 To 79
+                txtGrades.Text = "Good"
+
+            Case 60 To 69
+                txtGrades.Text = "medium"
+            Case 50 To 59
+                txtGrades.Text = "pass"
+            Case 0 To 49
+                txtGrades.Text = "Fail"
+            Case Else
+                txtGrades.Text = "Invalid Marks"
+        End Select
+
+    End Sub
+
+
+
+End Class
